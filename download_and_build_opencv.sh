40e31e12518cea3517b37b02ac01bdbc5af45f76
Gahan Saraiya <gahansaraiya@gmail.com>
2021-01-30 17:40:29 +0530

    Download and extract OpenCV and OpenCV-Contrib

---
+++ download_and_build_opencv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Construct Working directory and move to working directory
+WORKING_DIR=${PWD}
+export WORKING_DIR=${PWD}
+cd "${WORKING_DIR}"
+echo Working Directory is "${WORKING_DIR}"
+
+# Specify OpenCV version
+OPENCV_VERSION=4.5.1
+
+echo Downloading OpenCV-${OPENCV_VERSION}
+wget -O opencv.zip https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip
+echo Downloading OpenCV-Contrib-${OPENCV_VERSION}
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/${OPENCV_VERSION}.zip
+
+echo Extracting downloaded files...
+unzip opencv.zip
+unzip opencv_contrib.zip
+
+# Rename folder name to exclude version postfix
+mv opencv-${OPENCV_VERSION} opencv
+mv opencv_contrib-${OPENCV_VERSION} opencv_contrib
